02e9293ccfed4a32ee5928387d528226c56806e1
kuanoni <tehgamemaker@gmail.com>
2023-04-01 12:55:02 -0700

    rename tablatureStore "tablature" to "tablatureLines"

--- src/modules/tablatureStore/actions/insertBlankColumn.ts
+++ src/modules/tablatureStore/actions/insertBlankColumn.ts
@@ -3,7 +3,7 @@ import { tablatureStoreBase } from '../useTablatureStore';
 
 export const insertBlankColumn = (line: number, at: number) =>
 	tablatureStoreBase.setState((state) => {
-		if (!state.tablature[line]) return console.error(`Tried to insert column in non-existant line: ${line}`);
+		if (!state.tablatureLines[line]) return console.error(`Tried to insert column in non-existant line: ${line}`);
 
-		state.tablature[line].splice(at, 0, BLANK_COLUMN);
+		state.tablatureLines[line].splice(at, 0, BLANK_COLUMN);
 	});
