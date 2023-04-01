595e3ab930d9f1a473640129b3cd773c1a004138
kuanoni <tehgamemaker@gmail.com>
2023-04-01 11:30:30 -0700

    clean pushBlankColumn error checking

--- src/common/store/actions/pushBlankColumn.ts
+++ src/common/store/actions/pushBlankColumn.ts
@@ -3,6 +3,7 @@ import { tablatureStoreBase } from '../useTablatureStore';
 
 export const pushBlankColumn = (line: number) =>
 	tablatureStoreBase.setState((state) => {
-		if (state.tablature[line]) state.tablature[line].push(BLANK_COLUMN);
-		else console.error(`Tried to push column to non-existant line: ${line}`);
+		if (!state.tablature[line]) return console.error(`Tried to push column to non-existant line: ${line}`);
+
+		state.tablature[line].push(BLANK_COLUMN);
 	});
