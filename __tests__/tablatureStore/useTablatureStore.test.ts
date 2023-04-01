55fb9014b1a891f84f027532b3b3d03a3f1b832d
kuanoni <tehgamemaker@gmail.com>
2023-04-01 11:31:38 -0700

    Update useTablatureStore.test.ts

--- __tests__/tablatureStore/useTablatureStore.test.ts
+++ __tests__/tablatureStore/useTablatureStore.test.ts
@@ -1,4 +1,5 @@
 import { clearTablature } from '@common/store/actions/clearTablature';
+import { insertBlankColumn } from '@common/store/actions/insertBlankColumn';
 import { pushBlankColumn } from '@common/store/actions/pushBlankColumn';
 import { pushBlankLine } from '@common/store/actions/pushBlankLine';
 import { BLANK_COLUMN, BLANK_LINE, BLANK_TABLATURE } from '@common/store/constants';
@@ -45,5 +46,17 @@ describe('useTablatureStore', () => {
 		});
 
 		expect(result.current.tablature).toEqual([[...currentTablature[0], BLANK_COLUMN]]);
+	});
+
+	it('The insertBlankColumn action function correctly inserts a blank column in the first line.', () => {
+		const { result } = renderHook(() => useTablatureStore((state) => state));
+
+		const currentTablature = result.current.tablature;
+
+		act(() => {
+			insertBlankColumn(0, 0);
+		});
+
+		expect(result.current.tablature).toEqual([[BLANK_COLUMN, ...currentTablature[0]]]);
 	});
 });
