afe89c2df73f99494251559fd7f8cc35fa061397
kuanoni <tehgamemaker@gmail.com>
2023-04-03 12:06:15 -0700

    Create Instrument.ts

---
+++ src/common/Instrument.ts
@@ -0,0 +1,39 @@
+export class Instrument {
+	readonly name: string;
+	readonly amountOfStrings: number;
+	readonly amountOfFrets: number;
+	readonly defaultTuning: number[];
+	readonly commonTunings: number[][];
+	readonly BLANK_CELL: Cell;
+	readonly BLANK_COLUMN: Column;
+	readonly BLANK_LINE: Line;
+	readonly BLANK_TABLATURE: Tablature;
+
+	constructor(
+		name: string,
+		amountOfStrings: number,
+		amountOfFrets: number,
+		defaultTuning: number[],
+		commonTunings: number[][]
+	) {
+		this.name = name;
+		this.amountOfStrings = amountOfStrings;
+		this.amountOfFrets = amountOfFrets;
+		this.defaultTuning = defaultTuning;
+		this.commonTunings = commonTunings;
+		this.BLANK_CELL = { modifier: null, fret: -1 };
+		this.BLANK_COLUMN = {
+			modifier: null,
+			cells: new Array<Cell>(amountOfStrings).fill(this.BLANK_CELL),
+		};
+		this.BLANK_LINE = { columns: [this.BLANK_COLUMN, this.BLANK_COLUMN] };
+		this.BLANK_TABLATURE = { lines: [this.BLANK_LINE] };
+	}
+
+	createInitialState(): TablatureStore {
+		return {
+			instrument: this,
+			tablature: this.BLANK_TABLATURE,
+		};
+	}
+}
