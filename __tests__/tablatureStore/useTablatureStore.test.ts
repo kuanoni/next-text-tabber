438596ce238d6523f09f2d0d573bded949b610b4
kuanoni <tehgamemaker@gmail.com>
2023-04-03 12:53:48 -0700

    fix useTablatureStore tests

--- __tests__/tablatureStore/useTablatureStore.test.ts
+++ __tests__/tablatureStore/useTablatureStore.test.ts
@@ -3,7 +3,6 @@ import { clearTablature } from '@modules/tablatureStore/actions/clearTablature';
 import { insertBlankColumn } from '@modules/tablatureStore/actions/insertBlankColumn';
 import { pushBlankColumn } from '@modules/tablatureStore/actions/pushBlankColumn';
 import { pushBlankLine } from '@modules/tablatureStore/actions/pushBlankLine';
-import { BLANK_COLUMN, BLANK_LINE, BLANK_TABLATURE } from '@modules/tablatureStore/constants';
 import { useTablatureStore } from '@modules/tablatureStore/useTablatureStore';
 import { act, cleanup, renderHook } from '@testing-library/react';
 
@@ -17,46 +16,56 @@ describe('useTablatureStore', () => {
 	it('The clearTablature action function reverts the tablature to its original state.', () => {
 		const { result } = renderHook(() => useTablatureStore((state) => state));
 
+		const blankTablature = result.current.instrument.BLANK_TABLATURE;
+
 		act(() => {
 			clearTablature();
 		});
 
-		expect(result.current.tablatureLines).toEqual(BLANK_TABLATURE);
+		expect(result.current.tablature).toEqual(blankTablature);
 	});
 
 	it('The pushBlankLine action function correctly adds a blank line.', () => {
 		const { result } = renderHook(() => useTablatureStore((state) => state));
 
-		const currentTablature = result.current.tablatureLines;
+		const currentTablature = result.current.tablature;
+		const blankLine = result.current.instrument.BLANK_LINE;
+		const expected: Tablature = { lines: [...currentTablature.lines, blankLine] };
 
 		act(() => {
 			pushBlankLine();
 		});
 
-		expect(result.current.tablatureLines).toEqual([...currentTablature, BLANK_LINE]);
+		expect(result.current.tablature).toEqual(expected);
 	});
 
 	it('The pushBlankColumn action function correctly adds a blank column to the first line.', () => {
 		const { result } = renderHook(() => useTablatureStore((state) => state));
 
-		const currentTablature = result.current.tablatureLines;
+		const currentTablature = result.current.tablature;
+		const blankColumn = result.current.instrument.BLANK_COLUMN;
 
 		act(() => {
 			pushBlankColumn(0);
 		});
 
-		expect(result.current.tablatureLines).toEqual([[...currentTablature[0], BLANK_COLUMN]]);
+		expect(result.current.tablature).toEqual({
+			lines: [{ columns: [...currentTablature.lines[0].columns, blankColumn] }],
+		});
 	});
 
 	it('The insertBlankColumn action function correctly inserts a blank column in the first line.', () => {
 		const { result } = renderHook(() => useTablatureStore((state) => state));
 
-		const currentTablature = result.current.tablatureLines;
+		const currentTablature = result.current.tablature;
+		const blankColumn = result.current.instrument.BLANK_COLUMN;
 
 		act(() => {
 			insertBlankColumn(0, 0);
 		});
 
-		expect(result.current.tablatureLines).toEqual([[BLANK_COLUMN, ...currentTablature[0]]]);
+		expect(result.current.tablature).toEqual({
+			lines: [{ columns: [blankColumn, ...currentTablature.lines[0].columns] }],
+		});
 	});
 });
