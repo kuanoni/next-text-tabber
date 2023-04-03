8c0bcd4b2dc8baec6a9e578d529e04190d413d21
kuanoni <tehgamemaker@gmail.com>
2023-04-03 13:08:28 -0700

    add debug btns for changeInstrument action

--- src/modules/tablatureEditor/components/controls/TablatureControls.tsx
+++ src/modules/tablatureEditor/components/controls/TablatureControls.tsx
@@ -1,7 +1,9 @@
+import { changeInstrument } from '@modules/tablatureStore/actions/changeInstrument';
 import { clearTablature } from '@modules/tablatureStore/actions/clearTablature';
 import { insertBlankColumn } from '@modules/tablatureStore/actions/insertBlankColumn';
 import { pushBlankColumn } from '@modules/tablatureStore/actions/pushBlankColumn';
 import { pushBlankLine } from '@modules/tablatureStore/actions/pushBlankLine';
+import { electricBass, electricGuitar } from '@modules/tablatureStore/constants';
 
 import styles from './TablatureControls.module.scss';
 
@@ -12,6 +14,9 @@ const TablatureControls = () => {
 			<button onClick={() => insertBlankColumn(0, 0)}>insertBlankColumn</button>
 			<button onClick={() => pushBlankLine()}>pushBlankLine</button>
 			<button onClick={() => clearTablature()}>clearTablature</button>
+			<button onClick={() => clearTablature()}>clearTablature</button>
+			<button onClick={() => changeInstrument(electricGuitar)}>changeInstrument electricGuitar</button>
+			<button onClick={() => changeInstrument(electricBass)}>changeInstrument electricBass</button>
 		</div>
 	);
 };
