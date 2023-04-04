bc81f539fb4985245d5b4fb8c72746a1b750b5cc
kuanoni <tehgamemaker@gmail.com>
2023-04-04 07:45:49 -0700

    add default tunings

--- src/modules/tablatureStore/constants.ts
+++ src/modules/tablatureStore/constants.ts
@@ -12,5 +12,12 @@ export const COLUMN_MODIFIERS: { [i: string]: ColumnModifier } = {
 	Vibrato: { filler: '~' },
 };
 
-export const electricGuitar = new Instrument('Electric Guitar', 6, 24, [], [[]]);
-export const electricBass = new Instrument('Electric Bass', 4, 24, [], [[]]);
+const SIX_STRING_TUNINGS = {
+	'E Standard': [28, 33, 38, 43, 47, 52],
+	'Drop D': [28, 33, 38, 43, 47, 52],
+};
+
+export const electricGuitar = new Instrument('Electric Guitar', 6, 24, [28, 33, 38, 43, 47, 52], SIX_STRING_TUNINGS);
+export const electricBass = new Instrument('Electric Bass', 4, 24, [16, 21, 26, 31], {
+	'E Standard': [16, 21, 26, 31],
+});
