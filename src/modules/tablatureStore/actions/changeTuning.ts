6768b9d5413d4b024bb165136128c25cfcc3e76e
kuanoni <tehgamemaker@gmail.com>
2023-04-04 11:19:00 -0700

    add error throw for invalid tuning array size

--- src/modules/tablatureStore/actions/changeTuning.ts
+++ src/modules/tablatureStore/actions/changeTuning.ts
@@ -4,5 +4,9 @@ import { tablatureStoreBase } from '../useTablatureStore';
 
 export const changeTuning = (tuning: Instrument['defaultTuning']) =>
 	tablatureStoreBase.setState((state) => {
+		if (tuning.length !== state.instrument.defaultTuning.length)
+			throw new Error(
+				`[changeTuning]: invalid length of tuning array (${tuning.length}). Should be ${state.instrument.defaultTuning.length}.`
+			);
 		state.tuning = tuning;
 	});
