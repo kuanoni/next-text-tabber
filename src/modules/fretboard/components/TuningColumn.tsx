d6114d7fdca389556de1334c59e2a9a4746795c4
kuanoni <tehgamemaker@gmail.com>
2023-04-05 07:12:30 -0700

    add common tuning selector to TuningColumn

--- src/modules/fretboard/components/TuningColumn.tsx
+++ src/modules/fretboard/components/TuningColumn.tsx
@@ -1,3 +1,5 @@
+import { ChangeEventHandler } from 'react';
+
 import { changeTuning } from '@modules/tablatureStore/actions/changeTuning';
 import { useTablatureStore } from '@modules/tablatureStore/useTablatureStore';
 
@@ -6,8 +8,12 @@ import TuningSelector from './TuningSelector';
 
 const TuningColumn = () => {
 	const tuning = useTablatureStore().tuning;
+	const instrument = useTablatureStore().instrument;
 
-	const onSelectChange = (stringIndex: number, tuningValue: number) => {
+	const onCommonTuningChange: ChangeEventHandler<HTMLSelectElement> = (e) =>
+		changeTuning(instrument.commonTunings[e.target.value]);
+
+	const onTuningSelectorChange = (stringIndex: number, tuningValue: number) => {
 		const newTuning = [...tuning];
 		newTuning[stringIndex] = tuningValue;
 		changeTuning(newTuning);
@@ -15,12 +21,19 @@ const TuningColumn = () => {
 
 	return (
 		<div className={styles['tuning-column']}>
+			<select defaultValue={instrument.defaultTuningName} onChange={onCommonTuningChange}>
+				{Object.keys(instrument.commonTunings).map((tuningName) => (
+					<option key={tuningName} value={tuningName}>
+						{tuningName === instrument.defaultTuningName ? `${tuningName} (Default)` : tuningName}
+					</option>
+				))}
+			</select>
 			{tuning.map((tuningValue, stringIndex) => (
 				<TuningSelector
 					key={stringIndex}
 					stringIndex={stringIndex}
 					tuningValue={tuningValue}
-					onSelectChange={onSelectChange}
+					onSelectChange={onTuningSelectorChange}
 				/>
 			))}
 		</div>
