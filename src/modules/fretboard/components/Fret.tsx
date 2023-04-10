79d26a8126f07b9c3e1c0ba481ed218a19198877
kuanoni <tehgamemaker@gmail.com>
2023-04-10 09:34:42 -0700

    Create Fret.tsx

---
+++ src/modules/fretboard/components/Fret.tsx
@@ -0,0 +1,18 @@
+import { setColumnFret } from '@modules/tablatureStore/actions/setColumnFret';
+
+import styles from './Fretboard.module.scss';
+
+interface Props {
+	stringNumber: number;
+	fretNumber: number;
+}
+
+const Fret = ({ stringNumber, fretNumber }: Props) => {
+	const onClick = () => {
+		setColumnFret(stringNumber, fretNumber);
+	};
+
+	return <div className={styles.fret} onClick={onClick}></div>;
+};
+
+export default Fret;
