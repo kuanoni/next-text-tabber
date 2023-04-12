ea6d8c6bc066610333a97a04dba86e9c6ec7de5a
kuanoni <tehgamemaker@gmail.com>
2023-04-12 12:16:18 -0700

    rm start and end value ordering

--- src/modules/editorStore/actions/setColumnSelection.ts
+++ src/modules/editorStore/actions/setColumnSelection.ts
@@ -2,8 +2,6 @@ import { editorStoreBase } from '../useEditorStore';
 
 export const setColumnSelection = (line: number, start: number, end: number) => {
 	editorStoreBase.setState((state) => {
-		// swap values so start is always smaller than end
-		if (start > end) [start, end] = [end, start];
 		state.selectedColumns = { line, start, end };
 	});
 };
