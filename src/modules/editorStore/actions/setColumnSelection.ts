207c3822ca6a11ef8a68ddb11ee596b48ee9cacf
kuanoni <tehgamemaker@gmail.com>
2023-04-12 10:51:45 -0700

    rename selectColumn to setColumnSelection

--- src/modules/editorStore/actions/setColumnSelection.ts
+++ src/modules/editorStore/actions/setColumnSelection.ts
@@ -1,6 +1,6 @@
 import { editorStoreBase } from '../useEditorStore';
 
-export const selectColumns = (line: number, start: number, end: number) => {
+export const setColumnSelection = (line: number, start: number, end: number) => {
 	editorStoreBase.setState((state) => {
 		// swap values so start is always smaller than end
 		if (start > end) [start, end] = [end, start];
