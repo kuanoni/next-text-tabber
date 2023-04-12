e18c3e820dffbef90942f8d8c6631d39fca56125
kuanoni <tehgamemaker@gmail.com>
2023-04-12 12:15:53 -0700

    set isSelecting false on selection finish

--- src/modules/editorStore/actions/columnSelectionFinish.ts
+++ src/modules/editorStore/actions/columnSelectionFinish.ts
@@ -1,17 +1,7 @@
 import { editorStoreBase } from '../useEditorStore';
 
-export const columnSelectionFinish = (line: number, end: number) => {
+export const columnSelectionFinish = () => {
 	editorStoreBase.setState((state) => {
-		// if selection finishes on different line, or outside a column component
-		if (line !== state.selectedColumns.line || end < 0) {
-			state.selectedColumns = { ...state.selectedColumns, end: state.selectedColumns.start };
-			return;
-		}
-
-		let start = state.selectedColumns.start;
-		// swap values if necessary so start is always smaller than end
-		if (start > end) [start, end] = [end, start];
-
-		state.selectedColumns = { line, start, end };
+		state.isSelecting = false;
 	});
 };
