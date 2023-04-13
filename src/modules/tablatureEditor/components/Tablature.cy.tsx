5a18daf9af33268f5110a4cbc46a0127b21b3989
kuanoni <tehgamemaker@gmail.com>
2023-04-13 14:14:36 -0700

    add util functions to column selection tests

--- src/modules/tablatureEditor/components/Tablature.cy.tsx
+++ src/modules/tablatureEditor/components/Tablature.cy.tsx
@@ -1,15 +1,24 @@
+import TablatureControls from './controls/TablatureControls';
 import Tablature from './Tablature';
 
+const getTablature = () => cy.getTestElement('tablature');
+const getColumn = (columnIndex: number, lineIndex = 0) =>
+	cy.getTestElement('line').eq(lineIndex).find('[data-testid="column"]').eq(columnIndex);
 const columnIsSelected = (e: JQuery<HTMLElement>) => expect(e).to.have.attr('data-selected', 'true');
 const columnIsNotSelected = (e: JQuery<HTMLElement>) => expect(e).to.have.attr('data-selected', 'false');
 
 describe('Column selection', () => {
-	it('multiple columns can be selected with mouse events.', () => {
-		cy.mount(<Tablature />);
+	beforeEach(() => {
+		cy.mount(
+			<>
+				<Tablature />
+				<TablatureControls />
+			</>
+		);
+	});
 
-		const firstColumn = cy.getTestElement('line').getTestElement('column').first();
-
-		firstColumn
+	it('can select multiple columns from left-to-right.', () => {
+		getColumn(0)
 			.trigger('mousedown')
 			.should(columnIsSelected)
 			.next()
@@ -24,15 +33,37 @@ describe('Column selection', () => {
 			.should(columnIsNotSelected);
 	});
 
-	it('stops selecting when mouseup event is triggered outside column component.', () => {
-		cy.mount(<Tablature />);
-		const tablature = cy.getTestElement('tablature');
-		const firstColumn = cy.getTestElement('line').getTestElement('column').first().should(columnIsSelected);
+	it('can select mutliple columns from right-to-left.', () => {
+		getColumn(3)
+			.trigger('mousedown')
+			.should(columnIsSelected)
+			.prev()
+			.trigger('mouseover')
+			.should(columnIsSelected)
+			.prev()
+			.trigger('mouseover')
+			.trigger('mouseup')
+			.should(columnIsSelected)
+			.prev()
+			.trigger('mouseover')
+			.should(columnIsNotSelected);
+	});
 
-		firstColumn.trigger('mousedown').should(columnIsSelected);
+	it('stops selecting when "mouseup" event is triggered outside of Column component.', () => {
+		getColumn(0).trigger('mousedown').should(columnIsSelected);
+		getTablature().trigger('mouseover').trigger('mouseup');
+		getColumn(1).trigger('mouseover').should(columnIsNotSelected);
+	});
 
-		tablature.trigger('mouseover').trigger('mouseup');
+	it('clears the selection when the Tablature component is clicked.', () => {
+		getColumn(0).click().should(columnIsSelected);
+		getTablature().click();
+		getColumn(0).should(columnIsNotSelected);
+	});
 
-		firstColumn.next().trigger('mouseover').should(columnIsNotSelected);
+	it('prevents starting a selection on one line, and ending on another.', () => {
+		cy.getTestElement('pushBlankLine').click();
+		getColumn(0).trigger('mousedown').should(columnIsSelected);
+		getColumn(0, 1).trigger('mouseover').trigger('mouseup').should(columnIsNotSelected);
 	});
 });
