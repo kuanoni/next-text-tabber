c1c2359eaffcd74eda58497b02b77c367a998439
kuanoni <tehgamemaker@gmail.com>
2023-04-17 07:36:46 -0700

    rename "setColumnFret" to "setSelectedColumnsFret"

--- src/modules/tablatureStore/actions/setColumnFret.ts
+++
@@ -1,9 +0,0 @@
-import { tablatureStoreBase } from '../useTablatureStore';
-
-export const setColumnFret = (stringNumber: number, fretNumber: number) => {
-	console.log(stringNumber, ' | ', fretNumber);
-
-	// tablatureStoreBase.setState((state) => {
-	// 	state.tablature.lines.push(state.instrument.BLANK_LINE);
-	// });
-};
