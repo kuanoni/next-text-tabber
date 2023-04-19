a4d41b8b3e7dcd4cc3397c70c311ddfad93e6caf
kuanoni <tehgamemaker@gmail.com>
2023-04-19 07:51:17 -0700

    fix formatting

--- src/modules/tablatureEditorStore/editorSlice/actions/setColumnSelection.ts
+++ src/modules/tablatureEditorStore/editorSlice/actions/setColumnSelection.ts
@@ -1,7 +1,6 @@
 import { useTablatureEditorStore } from '../../useTablatureEditorStore';
 
-export const setColumnSelection = (line: number, start: number, end: number) => {
+export const setColumnSelection = (line: number, start: number, end: number) =>
 	useTablatureEditorStore.setState((state) => {
 		state.selectedColumns = { line, start, end };
 	});
-};
