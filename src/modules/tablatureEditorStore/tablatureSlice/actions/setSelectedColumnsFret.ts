3756dbe0d0bcdfaee1678a6575a6bc8d6fac0a67
kuanoni <tehgamemaker@gmail.com>
2023-04-19 07:50:56 -0700

    rm unnecessary geState()

--- src/modules/tablatureEditorStore/tablatureSlice/actions/setSelectedColumnsFret.ts
+++ src/modules/tablatureEditorStore/tablatureSlice/actions/setSelectedColumnsFret.ts
@@ -2,12 +2,11 @@ import { useTablatureEditorStore } from '@modules/tablatureEditorStore/useTablat
 
 import { iterateColumnSelection } from './utils/iterateColumnSelection';
 
-export const setSelectedColumnsFret = (stringNumber: number, fretNumber: number) => {
-	const { line } = useTablatureEditorStore.getState().selectedColumns;
-
+export const setSelectedColumnsFret = (stringNumber: number, fretNumber: number) =>
 	useTablatureEditorStore.setState((state) => {
+		const { line } = state.selectedColumns;
+
 		iterateColumnSelection((i) => {
 			state.tablature.lines[line].columns[i].cells[stringNumber].fret = fretNumber;
 		});
 	});
-};
