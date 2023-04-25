23fb2ad599f4d01bcb4661fa23aaeff01ef21391
kuanoni <tehgamemaker@gmail.com>
2023-04-25 14:21:45 -0700

    fix "selected columns" actions

--- src/modules/tablatureEditorStore/tablatureSlice/actions/clearSelectedColumns.ts
+++ src/modules/tablatureEditorStore/tablatureSlice/actions/clearSelectedColumns.ts
@@ -3,10 +3,8 @@ import { useTablatureEditorStore } from '@modules/tablatureEditorStore/useTablat
 import { iterateColumnSelection } from './utils/iterateColumnSelection';
 
 export const clearSelectedColumns = () =>
-	useTablatureEditorStore.setState((state) => {
-		const { line } = state.selectedColumns;
-
-		iterateColumnSelection((i) => {
-			state.tablature.lines[line].columns[i] = state.instrument.BLANK_COLUMN;
-		});
-	});
+	useTablatureEditorStore.setState((state) =>
+		iterateColumnSelection((i, currentSelection) => {
+			state.tablature.lines[currentSelection.line].columns[i] = state.instrument.BLANK_COLUMN;
+		})
+	);
