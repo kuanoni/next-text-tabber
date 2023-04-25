fe77daecb0f0e845fc8053f70563c7db6c7b4f79
kuanoni <tehgamemaker@gmail.com>
2023-04-25 14:20:45 -0700

    adjust column selection actions to use ghostSelection

--- src/modules/tablatureEditorStore/editorSlice/actions/columnSelectionFinish.ts
+++ src/modules/tablatureEditorStore/editorSlice/actions/columnSelectionFinish.ts
@@ -1,6 +1,21 @@
 import { useTablatureEditorStore } from '../../useTablatureEditorStore';
+import { BLANK_SELECTION } from '../constants';
 
 export const columnSelectionFinish = () =>
 	useTablatureEditorStore.setState((state) => {
+		const { ghostSelection } = state;
+
+		if (ghostSelection.line === null || ghostSelection.start === null || ghostSelection.end === null)
+			throw new Error(
+				`Attempted to finish selection with a null value, line=${ghostSelection.line} start=${ghostSelection.start}, end=${ghostSelection.end}`
+			);
+
+		const [start, end] =
+			ghostSelection.start <= ghostSelection.end
+				? [ghostSelection.start, ghostSelection.end]
+				: [ghostSelection.end, ghostSelection.start];
+
 		state.isSelecting = false;
+		state.currentSelection = { ...ghostSelection, start, end };
+		state.ghostSelection = BLANK_SELECTION;
 	});
--- src/modules/tablatureEditorStore/editorSlice/actions/columnSelectionStart.ts
+++ src/modules/tablatureEditorStore/editorSlice/actions/columnSelectionStart.ts
@@ -1,11 +1,13 @@
 import { useTablatureEditorStore } from '../../useTablatureEditorStore';
+import { BLANK_SELECTION } from '../constants';
 
 export const columnSelectionStart = (lineIndex: number, columnIndex: number) =>
 	useTablatureEditorStore.setState((state) => {
 		state.isSelecting = true;
-		state.selectedColumns = {
+		state.ghostSelection = {
 			line: lineIndex,
 			start: columnIndex,
 			end: columnIndex,
 		};
+		state.currentSelection = BLANK_SELECTION;
 	});
