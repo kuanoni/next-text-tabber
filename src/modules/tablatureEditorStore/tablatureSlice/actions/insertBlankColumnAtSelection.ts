07200a185792feb9e38860b0385cface471c683d
kuanoni <tehgamemaker@gmail.com>
2023-04-27 11:52:57 -0700

    fix insertBlankColumnAtSelection with new currentSelection

--- src/modules/tablatureEditorStore/tablatureSlice/actions/insertBlankColumnAtSelection.ts
+++ src/modules/tablatureEditorStore/tablatureSlice/actions/insertBlankColumnAtSelection.ts
@@ -3,8 +3,12 @@ import { insertColumns } from './utils/insertColumns';
 
 export const insertBlankColumnAtSelection = () =>
 	useTablatureEditorStore.setState((state) => {
-		const { line, end } = state.selectedColumns;
-		if (!state.tablature.lines[line]) return console.error(`Tried to insert column in non-existant line: ${line}`);
+		const { line, end } = state.currentSelection;
+
+		if (line === null || end === null)
+			return console.warn(`Attempting to insert column with null selection - line: ${line} - end: ${end}`);
+
+		if (!state.tablature.lines[line]) throw new Error(`Tried to insert column in non-existant line: ${line}`);
 
 		insertColumns(state, line, end, [state.instrument.BLANK_COLUMN]);
 	});
