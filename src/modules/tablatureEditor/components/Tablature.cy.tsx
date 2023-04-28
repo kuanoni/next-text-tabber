835317a38bae096e00db42a4d1e70243bade6bcc
kuanoni <tehgamemaker@gmail.com>
2023-04-28 12:36:08 -0700

    fix column selection Cypress tests

--- src/modules/tablatureEditor/components/Tablature.cy.tsx
+++ src/modules/tablatureEditor/components/Tablature.cy.tsx
@@ -5,6 +5,7 @@ const getTablature = () => cy.getTestElement('tablature');
 const getColumn = (columnIndex: number, lineIndex = 0) =>
 	cy.getTestElement('line').eq(lineIndex).find('[data-testid="column"]').eq(columnIndex);
 const columnIsSelected = (e: JQuery<HTMLElement>) => expect(e).to.have.attr('data-selected', 'true');
+const columnIsGhostSelected = (e: JQuery<HTMLElement>) => expect(e).to.have.attr('data-ghost-selected', 'true');
 const columnIsNotSelected = (e: JQuery<HTMLElement>) => expect(e).to.have.attr('data-selected', 'false');
 
 describe('Column selection', () => {
@@ -20,10 +21,10 @@ describe('Column selection', () => {
 	it('can select multiple columns from left-to-right.', () => {
 		getColumn(0)
 			.trigger('mousedown')
-			.should(columnIsSelected)
+			.should(columnIsGhostSelected)
 			.next()
 			.trigger('mouseover')
-			.should(columnIsSelected)
+			.should(columnIsGhostSelected)
 			.next()
 			.trigger('mouseover')
 			.trigger('mouseup')
@@ -36,10 +37,10 @@ describe('Column selection', () => {
 	it('can select mutliple columns from right-to-left.', () => {
 		getColumn(3)
 			.trigger('mousedown')
-			.should(columnIsSelected)
+			.should(columnIsGhostSelected)
 			.prev()
 			.trigger('mouseover')
-			.should(columnIsSelected)
+			.should(columnIsGhostSelected)
 			.prev()
 			.trigger('mouseover')
 			.trigger('mouseup')
@@ -50,7 +51,7 @@ describe('Column selection', () => {
 	});
 
 	it('stops selecting when "mouseup" event is triggered outside of Column component.', () => {
-		getColumn(0).trigger('mousedown').should(columnIsSelected);
+		getColumn(0).trigger('mousedown').should(columnIsGhostSelected);
 		getTablature().trigger('mouseover').trigger('mouseup');
 		getColumn(1).trigger('mouseover').should(columnIsNotSelected);
 	});
@@ -63,7 +64,7 @@ describe('Column selection', () => {
 
 	it('prevents starting a selection on one line, and ending on another.', () => {
 		cy.getTestElement('pushBlankLine').click();
-		getColumn(0).trigger('mousedown').should(columnIsSelected);
+		getColumn(0).trigger('mousedown').should(columnIsGhostSelected);
 		getColumn(0, 1).trigger('mouseover').trigger('mouseup').should(columnIsNotSelected);
 	});
 });
