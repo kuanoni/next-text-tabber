ca627ae451bbaf810cb4c9f4b5848fcbb4cbefa8
kuanoni <tehgamemaker@gmail.com>
2023-05-08 08:10:34 -0700

    render cells content as text instead of divs

--- src/modules/tablatureEditor/components/Column.tsx
+++ src/modules/tablatureEditor/components/Column.tsx
@@ -3,9 +3,9 @@ import { memo, MouseEventHandler } from 'react';
 import { columnSelectionFinish } from '@modules/tablatureEditorStore/editorSlice/actions/columnSelectionFinish';
 import { columnSelectionHover } from '@modules/tablatureEditorStore/editorSlice/actions/columnSelectionHover';
 import { columnSelectionStart } from '@modules/tablatureEditorStore/editorSlice/actions/columnSelectionStart';
+import { BLANK_NOTE_CHAR } from '@modules/tablatureEditorStore/tablatureSlice/constants';
 import { useTablatureEditorStore } from '@modules/tablatureEditorStore/useTablatureEditorStore';
 
-import Cell from './Cell';
 import styles from './Column.module.scss';
 
 interface Props {
@@ -47,9 +47,7 @@ const Column = memo<Props>(({ sectionIndex, columnIndex, column, isSelected, isG
 			onMouseDown={onMouseDown}
 			onMouseOver={onMouseOver}
 		>
-			{column.cells.map((cell, i) => (
-				<Cell key={i} cell={cell} />
-			))}
+			{column.cells.map((cell) => (cell.fret === -1 ? BLANK_NOTE_CHAR : cell.fret)).join('')}
 		</div>
 	);
 });
