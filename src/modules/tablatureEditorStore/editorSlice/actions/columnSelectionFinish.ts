0431ad4d95b9b586c3339d1e62c922127529fcda
kuanoni <tehgamemaker@gmail.com>
2023-05-09 08:25:48 -0700

    validate selection during selection actions

--- src/modules/tablatureEditorStore/editorSlice/actions/columnSelectionFinish.ts
+++ src/modules/tablatureEditorStore/editorSlice/actions/columnSelectionFinish.ts
@@ -1,14 +1,10 @@
 import { useTablatureEditorStore } from '../../useTablatureEditorStore';
 import { BLANK_SELECTION } from '../constants';
+import { validateColumnSelection } from './utils/validateColumnSelection';
 
 export const columnSelectionFinish = () =>
 	useTablatureEditorStore.setState((state) => {
-		const { ghostSelection } = state;
-
-		if (ghostSelection.section === null || ghostSelection.start === null || ghostSelection.end === null)
-			throw new Error(
-				`Attempted to finish selection with a null value, section=${ghostSelection.section} start=${ghostSelection.start}, end=${ghostSelection.end}`
-			);
+		const ghostSelection = validateColumnSelection(state.ghostSelection, state.tablature);
 
 		const [start, end] =
 			ghostSelection.start <= ghostSelection.end
--- src/modules/tablatureEditorStore/editorSlice/actions/columnSelectionHover.ts
+++ src/modules/tablatureEditorStore/editorSlice/actions/columnSelectionHover.ts
@@ -1,18 +1,12 @@
 import { useTablatureEditorStore } from '../../useTablatureEditorStore';
+import { validateColumnSelection } from './utils/validateColumnSelection';
 
 export const columnSelectionHover = (sectionIndex: number, columnIndex: number) =>
 	useTablatureEditorStore.setState((state) => {
 		// ignore if selection hovers on different section
 		if (sectionIndex !== state.ghostSelection.section) return;
 
-		const section = state.tablature.sections[sectionIndex];
-		if (!section) throw new Error(`Attempted to hover on out of bounds sectionIndex: ${sectionIndex}`);
+		const selection = validateColumnSelection({ ...state.ghostSelection, end: columnIndex }, state.tablature);
 
-		// if hovered columnIndex is somehow outside the bounds of the section's columns
-		if (columnIndex < 0 || columnIndex > section.columns.length - 1)
-			throw new Error(
-				`Attempted to hover on out of bounds columnIndex ${columnIndex} at sectionIndex ${sectionIndex}`
-			);
-
-		state.ghostSelection = { ...state.ghostSelection, end: columnIndex };
+		state.ghostSelection = selection;
 	});
