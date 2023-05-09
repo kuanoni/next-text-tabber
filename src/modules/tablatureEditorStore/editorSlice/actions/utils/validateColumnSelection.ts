bf96f2a8b015c18d9c3809448855388d1a56289e
kuanoni <tehgamemaker@gmail.com>
2023-05-09 08:24:45 -0700

    Create validateColumnSelection.ts

---
+++ src/modules/tablatureEditorStore/editorSlice/actions/utils/validateColumnSelection.ts
@@ -0,0 +1,24 @@
+export const validateColumnSelection = (selection: ColumnSelection, tablature: Tablature): ValidColumnSelection => {
+	const { section: sectionIndex, start, end } = selection;
+
+	if (sectionIndex === null) throw new Error(`Selected section is null.`);
+	if (start === null) throw new Error(`Selected start is null.`);
+	if (end === null) throw new Error(`Selected end is null.`);
+
+	if (sectionIndex < 0 || sectionIndex > tablature.sections.length - 1)
+		throw new Error(
+			`Selected section index ${sectionIndex} is out of bounds. Max ${tablature.sections.length - 1}`
+		);
+
+	const section = tablature.sections[sectionIndex];
+
+	if (!section) throw new Error(`Selected section at index ${sectionIndex} is non-existant: ${section}`);
+
+	if (start < 0 || start > section.columns.length - 1)
+		throw new Error(`Selected start index ${sectionIndex} is out of bounds. Max ${section.columns.length - 1}`);
+
+	if (end < 0 || end > section.columns.length - 1)
+		throw new Error(`Selected end index ${sectionIndex} is out of bounds. Max ${section.columns.length - 1}`);
+
+	return selection;
+};
