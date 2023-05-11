afd843ec65d4a204863cdfabe9cd94abc5cb58c8
kuanoni <tehgamemaker@gmail.com>
2023-05-11 11:33:07 -0700

    create replaceColumns util fn

---
+++ src/modules/tablatureEditorStore/tablatureSlice/actions/utils/replaceColumns.ts
@@ -0,0 +1,17 @@
+import { validateColumnSelection } from '@modules/tablatureEditorStore/utils/validateColumnSelection';
+
+import { insertColumns } from './insertColumns';
+
+export const replaceColumns = (state: TablatureEditorStore, selection: ColumnSelection, columnsToInsert: Column[]) => {
+	const { section, start, end } = validateColumnSelection(selection, state.tablature);
+	const selectionSize = end - start + 1;
+
+	if (start - end === 0) return insertColumns(state, selection, columnsToInsert);
+
+	state.tablature.sections[section].columns.splice(start, selectionSize, ...columnsToInsert);
+	const newSelection = validateColumnSelection(
+		{ section, start, end: start + columnsToInsert.length - 1 },
+		state.tablature
+	);
+	state.currentSelection = newSelection;
+};
