a95ebc7d133863153fe2b69605a1d0e0ab6f8936
kuanoni <tehgamemaker@gmail.com>
2023-05-11 09:09:17 -0700

    move validateColumnSelection to tablatureEditorStore dir

--- src/modules/tablatureEditorStore/editorSlice/actions/columnSelectionStart.ts
+++ src/modules/tablatureEditorStore/editorSlice/actions/columnSelectionStart.ts
@@ -1,6 +1,6 @@
 import { useTablatureEditorStore } from '../../useTablatureEditorStore';
+import { validateColumnSelection } from '../../utils/validateColumnSelection';
 import { BLANK_SELECTION } from '../constants';
-import { validateColumnSelection } from './utils/validateColumnSelection';
 
 export const columnSelectionStart = (sectionIndex: number, columnIndex: number) =>
 	useTablatureEditorStore.setState((state) => {
