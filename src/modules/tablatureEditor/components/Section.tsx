79b91aa888a636dcfa04174a327ae7d70bba97ea
kuanoni <tehgamemaker@gmail.com>
2023-05-16 10:11:35 -0700

    create getColumnModifierPosition type

--- src/modules/tablatureEditor/components/Section.tsx
+++ src/modules/tablatureEditor/components/Section.tsx
@@ -18,6 +18,21 @@ const isColumnInSelection = (selection: ColumnSelection, columnIndex: number, se
 	sectionIndex === selection.section &&
 	numIsBetweenRange(columnIndex, selection.start, selection.end);
 
+// Check adjacent columns to see if column with modifier is the start or end of the modified column group
+const getColumnModifierPosition = (columnIndex: number, columns: Column[]): ColumnModifierPosition => {
+	const column = columns[columnIndex];
+	if (!column.modifier) return undefined;
+
+	const prevColumn = columns[columnIndex - 1];
+	const nextColumn = columns[columnIndex + 1];
+
+	if (prevColumn?.modifier !== column.modifier && nextColumn?.modifier !== column.modifier) return 'solo';
+
+	if (prevColumn?.modifier !== column.modifier) return 'start';
+	else if (nextColumn?.modifier !== column.modifier) return 'end';
+	else return 'middle';
+};
+
 const Section = ({ sectionIndex, section }: Props) => {
 	const currentSelection = useTablatureEditorStore(currentSelectionSelector);
 	const ghostSelection = useTablatureEditorStore(ghostSelectionSelector);
@@ -27,13 +42,8 @@ const Section = ({ sectionIndex, section }: Props) => {
 			{section.columns.map((column, columnIndex) => {
 				const isSelected = isColumnInSelection(currentSelection, columnIndex, sectionIndex);
 				const isGhostSelected = isColumnInSelection(ghostSelection, columnIndex, sectionIndex);
-				let modifierPosition: keyof ColumnModifier | undefined;
+				const modifierPosition = getColumnModifierPosition(columnIndex, section.columns);
 
-				if (column.modifier) {
-					if (section.columns[columnIndex - 1]?.modifier !== column.modifier) modifierPosition = 'start';
-					else if (section.columns[columnIndex + 1]?.modifier !== column.modifier) modifierPosition = 'end';
-					else modifierPosition = 'filler';
-				}
 				return (
 					<Column
 						key={columnIndex}
