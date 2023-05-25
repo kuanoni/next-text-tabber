56c6d459b0e499363e44ae3f75ecae74dbf78cda
kuanoni <tehgamemaker@gmail.com>
2023-05-25 08:52:43 -0700

    impl deleteColumns in deleteSelectedColumns action

--- src/modules/editorStore/actions/deleteSelectedColumns.ts
+++ src/modules/editorStore/actions/deleteSelectedColumns.ts
@@ -1,17 +1,7 @@
 import { useTablatureEditorStore } from '../useTablatureEditorStore';
-import { validateColumnSelection } from '../utils/validateColumnSelection';
+import { deleteColumns } from './deleteColumns';
 
-export const deleteSelectedColumns = () =>
-	useTablatureEditorStore.setState((state) => {
-		const { section, start, end } = validateColumnSelection(state.currentSelection, state.tablature);
-		state.tablature.sections[section].columns.splice(start, end - start + 1);
-
-		// Prevent columns array from becoming empty
-		if (state.tablature.sections[section].columns.length === 0)
-			state.tablature.sections[section].columns = [state.instrument.BLANK_COLUMN];
-
-		// Reset currentSelection to prevent selection from being out of bounds
-		if (start > state.tablature.sections[section].columns.length - 1)
-			state.currentSelection = { section, start: start - 1, end: start - 1 };
-		else state.currentSelection = { section, start: start, end: start };
-	});
+export const deleteSelectedColumns = () => {
+	const currentSelection = useTablatureEditorStore.getState().currentSelection;
+	deleteColumns(currentSelection);
+};
