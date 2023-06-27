4b61af9ca4a74ae8cfc3d8478e13c26cd726bda6
kuanoni <tehgamemaker@gmail.com>
2023-06-26 20:31:35 -0700

    create index file for exporting actions

---
+++ src/modules/editorStore/new_actions/index.ts
@@ -0,0 +1,5 @@
+export { insertColumns } from './columns/insertColumns';
+export { appendColumns } from './columns/appendColumns';
+export { replaceColumns } from './columns/replaceColumns';
+export { deleteColumns } from './columns/deleteColumns';
+export { clearColumns } from './columns/clearColumns';
