564c751f4c1d5e10f21595d8468afae2e3b9ce38
kuanoni <tehgamemaker@gmail.com>
2023-06-27 23:01:26 -0700

    Update utils.ts

--- src/modules/editorStore/new_actions/utils.ts
+++ src/modules/editorStore/new_actions/utils.ts
@@ -1,24 +1,24 @@
 export const validateSelection = (selection: ColumnSelection, tablature: Tablature): NonBlankColumnSelection => {
-	const { section: sectionIndex, start, end } = selection;
+	const { section, start, end } = selection;
 
-	if (sectionIndex === null) throw new Error(`Selected section is null.`);
+	if (section === null && start === null && end === null) throw new Error('Selection is blank.');
+
+	if (section === null) throw new Error(`Selected section is null.`);
 	if (start === null) throw new Error(`Selected start is null.`);
 	if (end === null) throw new Error(`Selected end is null.`);
 
-	if (sectionIndex < 0 || sectionIndex > tablature.sections.length - 1)
-		throw new Error(
-			`Selected section index ${sectionIndex} is out of bounds. Max ${tablature.sections.length - 1}`
-		);
+	if (section < 0 || section > tablature.sections.length - 1)
+		throw new Error(`Selected section index ${section} is out of bounds. Max ${tablature.sections.length - 1}`);
 
-	const section = tablature.sections[sectionIndex];
+	const tablatureSection = tablature.sections[section];
 
-	if (!section) throw new Error(`Selected section at index ${sectionIndex} is non-existant: ${section}`);
+	if (!tablatureSection) throw new Error(`Selected section at index ${section} is non-existant: ${section}`);
 
-	if (start < 0 || start > section.columns.length - 1)
-		throw new Error(`Selected start index ${start} is out of bounds [0, ${section.columns.length - 1}]`);
+	if (start < 0 || start > tablatureSection.columns.length - 1)
+		throw new Error(`Selected start index ${start} is out of bounds [0, ${tablatureSection.columns.length - 1}]`);
 
-	if (end < 0 || end > section.columns.length - 1)
-		throw new Error(`Selected end index ${end} is out of bounds [0, ${section.columns.length - 1}]`);
+	if (end < 0 || end > tablatureSection.columns.length - 1)
+		throw new Error(`Selected end index ${end} is out of bounds [0, ${tablatureSection.columns.length - 1}]`);
 
 	return selection;
 };
