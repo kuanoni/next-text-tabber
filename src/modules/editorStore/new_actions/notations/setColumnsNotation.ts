045f316fcb2fd2de80ed9c0d6a1c22f9bb2c787a
kuanoni <tehgamemaker@gmail.com>
2023-06-27 22:57:51 -0700

    create notations actions

---
+++ src/modules/editorStore/new_actions/notations/setColumnsNotation.ts
@@ -0,0 +1,9 @@
+import { useEditorStore } from '../../useEditorStore';
+import { validateSelection } from '../utils';
+
+export const setColumnsNotation = (notation: ColumnNotation | null) =>
+	useEditorStore.setState((state) => {
+		const { section, start, end } = validateSelection(state.currentSelection, state.tablature);
+
+		for (let i = start; i < end; i++) state.tablature.sections[section].columns[i].notation = notation;
+	});
