9ce613ad4a03db595f9dc93da5964c55efc4b3d3
kuanoni <tehgamemaker@gmail.com>
2023-07-01 02:25:19 -0700

    fix deleting columns pushing selection out of bounds

--- src/modules/editorStore/actions/columns/deleteColumns.ts
+++ src/modules/editorStore/actions/columns/deleteColumns.ts
@@ -7,4 +7,18 @@ export const deleteColumns = () =>
 
 		const selectionSize = end - start + 1;
 		state.tablature.sections[section].columns.splice(start, selectionSize);
+
+		const columnsLen = state.tablature.sections[section].columns.length - 1;
+
+		if (start > columnsLen) {
+			// If selection start is out of bounds, set selection to the last column of the section...
+			state.currentSelection = {
+				section,
+				start: columnsLen,
+				end: columnsLen,
+			};
+		} else {
+			// ...otherwise, just shrink the selection size down to 1 column
+			state.currentSelection = { section, start, end: start };
+		}
 	});
